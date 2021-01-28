34e53bf5bf84116b6ba94823b2ff7ebc5ee8133c
mi.hambelov <mi.hambelov@tensor.ru>
2021-01-28 20:57:52 +0300

    https://online.sbis.ru/opendoc.html?guid=a8bbf07d-0aa6-497c-8634-7d79e365dffe

---
+++ test/AppUnit/State.test.ts
@@ -0,0 +1,75 @@
+import { default as DisposeControl, IResourceDisposable } from 'Application/_State/DisposeControl';
+import { assert } from 'chai';
+
+class CustomClass {
+    value: boolean = false;
+    numberVal: number = 0;
+
+    setValue(): void {
+        this.value = true;
+    }
+    getValue(): boolean {
+        return this.value;
+    }
+    clearValue(): void {
+        delete this.value;
+    }
+    setNumber(): void {
+        this.numberVal = 1;
+    }
+    getNumber(): number {
+        return this.numberVal;
+    }
+    clearNumber(): void {
+        delete this.numberVal;
+    }
+}
+class CustomResource implements IResourceDisposable {
+    enter(owner: CustomClass): void {
+        owner.setValue();
+    }
+    dispose(owner: CustomClass): void {
+        owner.clearValue();
+    }
+    getValue(owner: CustomClass): boolean {
+        return owner.getValue();
+    }
+}
+class CustomResourceNumber implements IResourceDisposable {
+    enter(owner: CustomClass): void {
+        owner.setNumber();
+    }
+    dispose(owner: CustomClass): void {
+        owner.clearNumber();
+    }
+    getValue(owner: CustomClass): number {
+        return owner.getNumber();
+    }
+}
+const class1 = new CustomClass();
+describe('Application/_State', () => {
+    describe('DisposeControl', () => {
+        it('Проверка работы с одним ресурсом', () => {
+            const resources = new DisposeControl(class1);
+            const resourcesItem1 = new CustomResource();
+            resources.track(resourcesItem1);
+            assert.isTrue(resourcesItem1.getValue(class1),
+                'Ресурс не был активирован. Проверяемое свойство не изменено');
+            resources.dispose();
+            assert.isUndefined(resourcesItem1.getValue(class1),
+                'Ресурс не был освобожден. Проверяемое свойство не было удалено');
+        });
+        it('Проверка работы используя несколько ресурсов', () => {
+            const resources = new DisposeControl(class1);
+            const resourcesItem1 = new CustomResource();
+            const resourcesItem2 = new CustomResourceNumber();
+            resources.track(resourcesItem1);
+            resources.track(resourcesItem2);
+            resources.dispose();
+            assert.isUndefined(resourcesItem1.getValue(class1),
+                'Ресурс1 не был освобожден. Проверяемое свойство не было удалено');
+            assert.isUndefined(resourcesItem2.getValue(class1),
+                'Ресурс2 не был освобожден. Проверяемое свойство не было удалено');
+        });
+    });
+});
