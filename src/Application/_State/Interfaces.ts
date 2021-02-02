e94c95a34fe7554ae5805e49047ac6140d8207b4
mi.hambelov <mi.hambelov@tensor.ru>
2021-02-02 16:12:44 +0300

    https://online.sbis.ru/opendoc.html?guid=ea559118-f33d-4d73-8559-b701786fb40b

--- src/Application/_State/Interfaces.ts
+++ src/Application/_State/Interfaces.ts
@@ -1,9 +1,19 @@
 /**
  * Интерфейс ресурса
+ * @interface Application/_State/IResourceDisposable
+ * @public
+ * @author Хамбелов М.И.
  */
+
 export interface IResourceDisposable {
-    /** Открыть ресурс */
+    /**
+     * открывает ресурс
+     * @param {unknown} owner контрол, которому принадлежит ресурс
+     */
     enter(owner: unknown): void;
-    /** Освободить ресурс */
+    /**
+     * закрывает ресурс
+     * @param {unknown} owner контрол, которому принадлежит ресурс
+     */
     dispose(owner: unknown): void;
 }
