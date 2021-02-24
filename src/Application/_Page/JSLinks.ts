5963d962f4275e4cfe25cd6a2bb35583c639f592
mi.hambelov <mi.hambelov@tensor.ru>
2021-02-24 14:59:21 +0300

    https://online.sbis.ru/opendoc.html?guid=dffff7ff-3a50-429c-a0c9-146d553372b1

--- src/Application/_Page/JSLinks.ts
+++ src/Application/_Page/JSLinks.ts
@@ -1,40 +1,70 @@
-import { Head as HeadAPI, Body as BodyAPI } from 'Application/Page'
-import { IHeadTagEventHandlers, IHeadTagId} from "Application/_Interface/IHead";
-import AppEnv from "Application/Env";
+import AppEnv from 'Application/Env';
+import { Head as HeadAPI } from 'Application/Page'
+import {
+    IHeadTag,
+    IHeadTagAttrs,
+    IHeadTagEventHandlers,
+    IHeadTagId,
+    // JML
+} from 'Application/_Interface/IHead';
+import { IStore } from "Application/_Interface/IStore";
 
-interface IJSLinks{
-
+type JSLinksTag = IHeadTag;
+type JSLinksTagId = string;
+type IJSLinksTagEventHandlers = IHeadTagEventHandlers;
+interface IJSLinks extends IStore<IJSLinks> {
+    createTag(name: string, attrs: IHeadTagAttrs, content?: string, eventHandlers?: IHeadTagEventHandlers): IHeadTagId;
+    // deleteTag(id: IHeadTagId): void;
+    // getTag(name?: string, attrs?: IHeadTagAttrs): IHeadTagId | IHeadTagId[] | null;
+    // getData(id?: IHeadTagId): Array<JML> | JML;
 }
-export default class JSLinks extends HeadAPI implements IJSLinks{
-    private _JSLink_items = {};
 
+export default class JSLinks extends HeadAPI implements IJSLinks{
+    private _elementsJSLinks: {[propName: string]: JSLinksTag } = {};
+    private _id = 0;
     createTag(
         name: 'script',
-        attrs: {type: string},
-        content: string): IHeadTagId;
+        attrs: {src: string}): JSLinksTagId;
     createTag(
         name: 'script',
-        attrs: {type: string, src: string, key: string},
-        content: string): IHeadTagId;
+        attrs: {},
+        content: string): JSLinksTagId;
     createTag(
         name: 'script',
-        attrs: {type: string, src: string, key: string},
-        content: string,
-        eventHandlers: IHeadTagEventHandlers): IHeadTagId {
-        // const elementClass = typeof window === 'undefined' ? ElementPS : Element;
-        // const uuid = this._generateGuid();
-        // this._elements[uuid] = new elementClass(name, attrs, content, eventHandlers);
+        attrs: {type: string, src: string}): JSLinksTagId;
+    createTag(
+        name: 'script',
+        attrs: {type: string, src: string, defer: string}): JSLinksTagId;
+    createTag(
+        name: 'script',
+        attrs: {type: string, src: string, defer: string},
+        content?: string,
+        eventHandlers?: IJSLinksTagEventHandlers): JSLinksTagId {
+        const uuid = this._generateGuid();
+        /**
+         * при работе с rsSerialized, rtpackModuleNames пробрасывается только content, аттрибуты не требуются.
+         * поэтому если контента нету, значит пробрасываем аттрибуты и дополняем необходимые, если они не пришли
+         */
+        if (!content) {
+            attrs = {
+                ...attrs,
+                type: attrs.type ? attrs.type : 'text/javascript' ,
+                defer:  attrs.defer ? attrs.defer : 'defer'
+            }
+        }
+        this._elementsJSLinks[uuid] = {name, attrs, content, eventHandlers};
+        return uuid;
     }
+
+    private _generateGuid(): IHeadTagId {
+        return `scripts-${this._id++}`;
+    };
     private static _creator(): JSLinks {
-        return new JSLinks();
+        return new this();
     }
     static _instance: JSLinks;
 
-    static getInstance(nameSpace: string = 'root'): JSLinks | never {
-        if (typeof window !== 'undefined') {
-            JSLinks._instance = JSLinks._instance || JSLinks._creator();
-            return JSLinks._instance;
-        }
-        return <JSLinks> AppEnv.getStore(`JSLinks${nameSpace}`, JSLinks._creator);
+    static getInstance(nameSpace: string = 'root'): IJSLinks {
+        return <IJSLinks> AppEnv.getStore(`JSLinksAPI_${nameSpace}`, JSLinks._creator);
     }
 }
