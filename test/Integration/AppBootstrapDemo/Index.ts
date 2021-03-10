a4eaef76395646baf62acd0c921688f9af539e70
mi.hambelov <mi.hambelov@tensor.ru>
2021-03-10 15:59:49 +0300

    https://online.sbis.ru/opendoc.html?guid=cf2d721c-36fd-44bb-9a19-c02763557ebd

---
+++ test/Integration/AppBootstrapDemo/Index.ts
@@ -0,0 +1,11 @@
+import { Control, TemplateFunction } from 'UI/Base';
+// @ts-ignore
+import * as template from 'wml!AppBootstrapDemo/Index';
+
+/**
+ * Точка входа для демонстрации AppBootstrapDemo
+ */
+
+export default class Index extends Control {
+    protected _template: TemplateFunction = template;
+}
