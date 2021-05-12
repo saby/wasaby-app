0da2de157f4d1f9cdb9f12a6d7108c1a5802e558
mi.hambelov <mi.hambelov@tensor.ru>
2021-05-12 15:57:00 +0300

    https://online.sbis.ru/opendoc.html?guid=9d605b23-d688-4e39-9d61-b7ea6c999494

---
+++ src/Application/Interface.ts
@@ -0,0 +1,13 @@
+/// <amd-module name="Application/Interface" />
+
+/**
+ * Библиотека интерфейсов
+ * @deprecated
+ * @library Application/Interface
+ * @public
+ * @author Санников К.А.
+ */
+
+export { ILocation } from 'Application/_Env/ILocation';
+export { ISerializableState, IStateReceiver } from 'Application/_State/Interfaces';
+export * from 'Application/_Request/IStore';
