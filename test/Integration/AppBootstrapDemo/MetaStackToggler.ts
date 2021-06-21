251562baaf2099e515079f9e19710000d840c70d
mi.hambelov <mi.hambelov@tensor.ru>
2021-06-21 15:56:01 +0300

    https://online.sbis.ru/opendoc.html?guid=b74bb8b0-bff8-449c-9911-a7c7eea6c70e metastack

--- test/Integration/AppBootstrapDemo/MetaStackToggler.ts
+++ test/Integration/AppBootstrapDemo/MetaStackToggler.ts
@@ -8,15 +8,21 @@ import { IMetaState } from 'UI/_base/HTML/meta';
 export default class MetaStackToggler extends Control {
     _template: TemplateFunction = template;
     _metaState: IMetaState = null;
+    private _counter: number = 0;
+    /** увеличить счетчик и получить его */
+    _getIncreasedCounter(): number {
+        this._counter = this._counter + 1;
+        return this._counter;
+    }
     _updateTitle(): void {
-        this._metaState = getMetaStack().push({title: `New Title: ${Math.random()}`});
+        this._metaState = getMetaStack().push({title: `New Title: ${this._getIncreasedCounter()}`});
     }
     _restoreTitle(): void {
         try {
             getMetaStack().remove(this._metaState);
             this._metaState = null;
         }
-            // tslint:disable-next-line:no-empty
+        // tslint:disable-next-line:no-empty
         catch (_) {}
     }
 }
