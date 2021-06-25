2995877eb64c08738811c334b8f86d8cf14d6221
mi.hambelov <mi.hambelov@tensor.ru>
2021-06-25 13:10:55 +0300

    https://online.sbis.ru/opendoc.html?guid=2684f66c-6de5-4507-a9e4-674a2ef45e61

--- src/Application/_Page/_jslinks/IJSLinks.ts
+++ src/Application/_Page/_jslinks/IJSLinks.ts
@@ -1,25 +1,29 @@
+// tslint:disable:max-line-length
+
 import type { IHeadTagAttrs, IHeadTagEventHandlers, IHeadTagId, IInternalHead, JML } from 'Application/_Page/_head/IHead';
 import type { IStore } from 'Application/_Request/IStore';
 
 export type JSLinksTagId = string;
-
 /**
- * API для работы с JSLinks
- * @interface Application/_Page/_jslinks/IJSLinks
- * @property {Function} createTag - добавит jslinks
- * @property {Function} getTag
- * переопределенный метод от Head, в котором параметр name всегда должен быть script.
- * Вернет описание тега(ов), если он есть по входным данным: имя тега script и аттрибуты
- * @property {Function} getData - вернет текущее состояние тегов в формате JsonML
- * @public
+ * Внутренний интерфейс IJSLinks, содержит весь основной функционал
+ * @private
  * @author Хамбелов М.И.
  */
 export interface IJSLinksInternal extends IInternalHead {
-    // tslint:disable-next-line:max-line-length
+    /** Добавит jslinks  */
     createTag(name: 'script', attrs: IHeadTagAttrs, content?: string, eventHandlers?: IHeadTagEventHandlers): IHeadTagId;
+    /**
+     * Переопределенный метод от Head, в котором параметр name всегда должен быть script.
+     * Вернет описание тега(ов), если он есть по входным данным: имя тега script и аттрибуты
+     */
     getTag(name?: 'script', attrs?: IHeadTagAttrs): JSLinksTagId | JSLinksTagId[] | null;
+    /** Вернет текущее состояние тегов в формате JsonML */
     getData(id?: IHeadTagId): JML[] | JML;
 }
 export type KeyJSLinksInternal = keyof IJSLinksInternal;
-
+/**
+ * API для работы с JSLinks
+ * @public
+ * @author Хамбелов М.И.
+ */
 export interface IJSLinks extends IJSLinksInternal, IStore<IJSLinksInternal> {}
